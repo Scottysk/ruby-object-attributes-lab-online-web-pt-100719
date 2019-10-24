e755a6a7e9a494bdca5cbe130e1e0d7c5dcefb87
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-24 15:22:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
   
-  def name(dog_name)
-    @name = dog_name
-  end
-  
   def name
     @name
   end
   
-  def breed(new_breed)
-    @breed = new_breed
+  def name=(dog_name)
+    @name = dog_name
   end
   
   def breed
     @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
   end
   
   
